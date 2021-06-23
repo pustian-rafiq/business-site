a958e5b9c7025ee449fb295088954b5b0166a072
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-06-23 22:29:24 +0600

    add owl carousel

---
+++ assets/js/main.js
@@ -0,0 +1,9 @@
+// Owl Carosel Js code
+$(document).ready(function(){
+    $(".owl-carousel").owlCarousel({
+        'items': 1,
+        'loop':true,
+        'autoplay':true,
+        'autoplaySpeed':true
+    });
+  });
