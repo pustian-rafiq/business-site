b51778707d7ff1d0eab4bd5fb3385ae5f9cb247c
Md.Rafiqul <rafiqul.pust.cse@gmail.com>
2021-06-28 19:23:43 +0600

    add counter up section

--- assets/js/main.js
+++ assets/js/main.js
@@ -1,9 +1,16 @@
 // Owl Carosel Js code
 $(document).ready(function(){
+    // Mian slider activation
     $(".owl-carousel").owlCarousel({
         'items': 1,
         'loop':true,
         'autoplay':true,
         'autoplaySpeed':true
+    });
+
+    // Counter up 
+    $('.counter').counterUp({
+        delay: 10,
+        time: 1000
     });
   });
